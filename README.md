636ab7b8e76f84d6989ca773fc15089d1def7252
Peh_Xuan_He <23017469@myrp.edu.sg>
2025-07-01 13:38:04 +0800

    Final Bug patch fr fr

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 guess.html is the updated one i uploaded 2 files so we can always double back
+
+List of fixed bugs:
+
+Typing the first letter of the key word triggers correct guess 
+Typing the first letter of the key word triggers correct guess as a card holder
+
+Typing the Key word triggers correct guess as a card holder
+Typing the forbidden word does not trigger the forbidden word (as guesser and card holder)
+
+People from different team being able to speak when not their turn (Both Mic and chat)
+
+After keyword has been guessed, the keyword can be entered again to score more points.
+
+List of known bugs:
+--None so far. fill in later if theres more
+
+changed:
+Changed the word checking system. Old code is still inside just uncomment it if you want to change something
--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const express = require('express');
 const http = require('http');
+const { type } = require('os');
 const socketIO = require('socket.io');
+const { json } = require('stream/consumers');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-//const hostname = '10.175.4.149'; 
+//Uncomment the code below if you want to allow other devices to enter the site. Replace the ip address with your own devices name.
+const hostname = '10.175.2.91'; 
+// ^^^ above is my devices ip when connected to RP's wifi. replace the 10.175.4.149
 //const hostname = '192.168.1.1'; 
+// ^^^ above is my devices ip when connected to My own wifi. replace the 192.168.1.1
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/Taboo.html");
 });
 
-app.get("/taboo2", (req, res) => {
-  res.sendFile(__dirname + "/taboo2.html");
-});
+
+  app.get("/taboo2", (req, res) => {
+    res.sendFile(__dirname + "/taboo2.html");
+  })
+
+  app.get("/Unsupported_Browser",(req,res) => {
+    res.sendFile(__dirname + "/Unsupported_Browser.html")
+  })
+
+
+
 
 app.get("/taboo3", (req, res) => {
   res.sendFile(__dirname + "/taboo3.html");
 });
+
+
 
 // Cards
 const cards = [
@@ -53,7 +68,7 @@ let maxRounds = 5;
 let currentTeamTurn = "Team_A"
 
 
-
+//sets currentTime
 //let currentTime = 5;
 
 function Card_RNG() {
@@ -64,28 +79,12 @@ function Card_RNG() {
   return Current_Card;
 }
 
-//function endRound() {
-  //Current_Card = null;
-  //currentRound++;
-
-    //if (currentRound > maxRounds) {
-      //io.emit("Game_Over");
-      //console.log("Game has ended.");
-      //return;
-    //}
-
-    //currentTeamTurn = currentTeamTurn === "Team_A" ? "Team_B" : "Team_A";
-    //io.emit("Next_Round", {
-      //round: currentRound,
-      //team: currentTeamTurn
-    //});
-//}
 
 
-
-
+//function called endRound was here. deleted since it was useless. if needed check previous versions
 
 io.on("connection", (socket) => {
+  
   
   let assignedTeam;
  
@@ -104,6 +103,7 @@ io.on("connection", (socket) => {
       socket.join("Team_A");
       const assignedTeam = "Team_A";
       socket.emit("Team", (assignedTeam))
+      Team_A=Team_A
       const A1 = Team_A[0];
       if (Card_Holder.includes(A1) === false) {
         Card_Holder.push(A1)
@@ -121,6 +121,7 @@ io.on("connection", (socket) => {
       socket.join("Team_B");
       const assignedTeam = "Team_B";
       socket.emit("Team", (assignedTeam))
+      Team_B=Team_B
       const B1 = Team_B[0];
       if (Card_Holder.includes(B1) === false) {
         Card_Holder.push(B1)
@@ -156,56 +157,134 @@ io.on("connection", (socket) => {
     if (currentRound%2 == 1){
       io.emit("Team_A_Turn" , currentRound)
       console.log("Current Round: ",currentRound)
+      //Used to return the current round. left in since could be useful
       //return currentRound;
     } else {
       io.emit("Team_B_Turn" , currentRound)
       console.log("Current Round: ",currentRound)
+      //Used to return the current round. left in since could be useful
       //return currentRound;
     }
     
   };
 
   function Word_Checker(capitalizedWord) {
-    if (guesser.includes(socket.id)){
-        if (Current_Card.forbidden == capitalizedWord) { //make it so that it capitalizes the msg first
-          console.log(capitalizedWord, "Is a forbidden word")
-          socket.emit("Forbidden_Word_Used")
+    const Keyword = Current_Card.keyword
+    var forbidden_words = []
+    var forbidden_words = Current_Card.forbidden.slice()
+    Current_Card.forbidden.forEach((check) => {
+      //var forbidden_words = Current_Card.forbidden
+      //console.log("This is a loop message for the forbidden word")
+      const current_card_fw_string =JSON.stringify(forbidden_words.shift())
+      //console.log("The stringified fw",current_card_fw_string)
+      if(current_card_fw_string === JSON.stringify(capitalizedWord)) {
+        console.log(capitalizedWord, "Is a forbidden word");
+          socket.emit("Forbidden_Word_Used");
           stoptimer();
           currentRound++;
-          isEven(currentRound)
-          Current_Card = null
-          
-        }else if (Current_Card.keyword == capitalizedWord) {
-          socket.emit("Correct_Answer");
-          //const playerTeam = Team_A.includes(socket.id) ? "Team_A" : "Team_B";
-          //scores[playerTeam] += 1;
-          currentRound++;
-          stoptimer()
-          isEven(currentRound)
-          Current_Card = null
-          //socket.emit("Score_Update", scores)
-        }
-    } else if (Card_Holder.includes(socket.id)){
-        if (Current_Card.forbidden == capitalizedWord) { //make it so that it capitalizes the msg first
-          console.log(capitalizedWord, "Is a forbidden word")
-          socket.emit("Forbidden_Word_Used")
-          stoptimer();
-          currentRound++;
-          isEven(currentRound)
-          Current_Card = null
-        }else if (Current_Card.keyword == capitalizedWord) {
+          isEven(currentRound);
+          Current_Card = null;
+      }
+    })
+
+    //var forbidden_words = Current_Card.forbidden
+
+    if (capitalizedWord === Keyword) {
+      var card_holder_list =  Card_Holder.slice()
+      const current_guesser_id_checked = JSON.stringify(guesser.shift())
+      Card_Holder.some((check_id_role) => {
+        const current_card_holder_id_checked = JSON.stringify(card_holder_list.shift())
+        console.log("Test if this statement is true",current_card_holder_id_checked === JSON.stringify(socket.id))
+
+        if (current_card_holder_id_checked === JSON.stringify(socket.id)) {
+          //console.log("Test if this statement is true",current_card_holder_id_checked === socket.id)
           socket.emit("Said_Key_Word");
           console.log(socket.id,"Said the key word. Word said was:",capitalizedWord)
-          //const playerTeam = Team_A.includes(socket.id) ? "Team_A" : "Team_B";
-          //scores[playerTeam] += 1;
+          //two lines of code were here that were used to assign teams and points before the new system. deleted.
           currentRound++;
-          stoptimer()
-          isEven(currentRound)
-          Current_Card = null
-          //socket.emit("Score_Update", scores)
-        }
+          stoptimer();
+          isEven(currentRound);
+          Current_Card = null;
+          return true
+        } 
 
+        return false;
+      })
+
+      if (current_guesser_id_checked === JSON.stringify(socket.id)) {
+          socket.emit("Correct_Answer");
+          //two lines of code were here that were used to assign teams and points before the new system. deleted.
+          currentRound++;
+          stoptimer();
+          isEven(currentRound);
+          Current_Card = null;
+          //Code was here to emit score update event. deleted.
+        }
     }
+    
+    //const forbidden = Current_Card.forbidden
+    //const Keyword = Current_Card.keyword
+    //console.log("The capitalized word is:",capitalizedWord)
+    //console.log("Check if the user is in guesser list:",guesser.includes(socket.id))
+    //console.log("Check if the user is in card holder list:",Card_Holder.includes(socket.id))
+    //console.log("Current Card:",Current_Card)
+    //console.log("Global check keyword:", Keyword.includes(capitalizedWord))
+    //console.log("Global check Forbidden:", forbidden.includes(capitalizedWord))
+    //console.log(guesser)
+    //console.log(Card_Holder)
+////////if//((Card_Holder.includes(socket.id))){
+////////////console.log("The//person//who//just//chatted//is//a//guesser");
+////////////console.log("True/False//value//for//forbidden//word//(Guesser)",forbidden//==//capitalizedWord)
+////////////console.log("True/False//value//for//Key//word//Guesser)",Current_Card.keyword==capitalizedWord)
+////////////////if//(Current_Card.forbidden.includes(capitalizedWord))//{////make//it//so//that//it//capitalizes//the//msg//first//DONE\\
+////////////////////console.log(capitalizedWord,//"Is//a//forbidden//word");
+////////////////////socket.emit("Forbidden_Word_Used");
+////////////////////stoptimer();
+////////////////////currentRound++;
+////////////////////isEven(currentRound);
+////////////////////Current_Card//=//null;
+////////////////////
+////////////////}else//if//(Current_Card.keyword.includes(capitalizedWord))//{
+////////////////////
+////////////////////socket.emit("Correct_Answer");
+//////////////////////two//lines//of//code//were//here//that//were//used//to//assign//teams//and//points//before//the//new//system.//deleted.
+////////////////////currentRound++;
+////////////////////stoptimer();
+////////////////////isEven(currentRound);
+////////////////////Current_Card//=//null;
+//////////////////////Code//was//here//to//emit//score//update//event.//deleted.
+////////////////
+////////////////}
+////////////////
+////////////////
+////////}else//if//(Card_Holder.includes(socket.id)){
+////////////console.log(Current_Card.forbidden.includes(capitalizedWord))
+////////////console.log("The//person//who//just//chatted//is//a//card//holder")
+////////////console.log("True/False//value//for//forbidden//word//(card_Holder)",Current_Card.forbidden//===//capitalizedWord)
+////////////console.log("True/False//value//for//Key//word//(card_Holder)",Current_Card.keyword//===//capitalizedWord//)
+////////////////if//(Current_Card.forbidden.includes(capitalizedWord))//{////make//it//so//that//it//capitalizes//the//msg//first////DONE//\\
+////////////////////console.log(capitalizedWord,//"Is//a//forbidden//word")
+////////////////////socket.emit("Forbidden_Word_Used")
+////////////////////stoptimer();
+////////////////////currentRound++;
+////////////////////isEven(currentRound);
+////////////////////Current_Card//=//null;
+////////////////}else//if//(Current_Card.keyword.includes(capitalizedWord))//{
+////////////////////socket.emit("Said_Key_Word");
+////////////////////console.log(socket.id,"Said//the//key//word.//Word//said//was:",capitalizedWord)
+//////////////////////two//lines//of//code//were//here//that//were//used//to//assign//teams//and//points//before//the//new//system.//deleted.
+////////////////////currentRound++;
+////////////////////stoptimer();
+////////////////////isEven(currentRound);
+////////////////////Current_Card//=//null;
+//////////////////////code//was//here//that//was//used//to//trigger//a//previous//socket//emmiter.//check//versions//of//code.
+////////////////}
+////////////////
+//////////////////
+
+////////}
+    
+    
     io.to("Team_A").to("Team_B").emit("card_drawn",Current_Card)
   }
   
@@ -224,25 +303,8 @@ io.on("connection", (socket) => {
 
   console.log(`${socket.id} has joined ${assignedTeam}`);
   console.log("These sockets are in the Card_Holder Roll:",Card_Holder)
-  console.log("These sockets are in the Card_Holder Roll:",guesser)
-  // assigning roles based on first player of each team ## change this to be random after round feature is implemented
-  //if (Team_A.length > 0) {
-    
-    
-  //}
-
-  //if (Team_B.length > 0) {
-    
-  //}
-
-  // makes everyone else into guessers
-  //if (Card_Holder.includes(socket.id) === false) {
-    //guesser.push(socket.id);
-    //role = "guesser"  
-    //socket.emit("role_assigned" , role);
-    //console.log(guesser)
-  //}
-
+  console.log("These sockets are in the Guesser Roll:",guesser)
+  //lines of code here to assign clients to teams. depreciated and deleted.
 
   const Team_Player_Count = {
     Team_A_Count: Team_A.length,
@@ -252,7 +314,7 @@ io.on("connection", (socket) => {
   };
 
   socket.on("Team_Chosen_Name", Team_Chosen_Name =>{
-    //Team_Name.push(Team_A)
+    //line of code was here to push name. deleted and redundant.
     
     io.emit("Team_Names",(Team_Chosen_Name))
     Team_Name = Team_Chosen_Name
@@ -260,27 +322,15 @@ io.on("connection", (socket) => {
     isEven(currentRound)
   })
 
-  //socket.on("NameDecided",namedecided => {
-    
-  //})
-  //socket.on("Team_B_Name", (Team_B) =>{
-    //Team_Name.push(Team_B)
-    //socket.emit("Team_Names",(Team_Name))
-  //})
-
-  //if (Team_Name.length == 2) {
-    //socket.emit("Team_Names",(Team_Name))
-  //}
-
-
-  
-  
+  //depreciated code that was used to decide team name. replaced. check previous versions.  
 
   socket.on("draw_card" , () =>{
 
     Card_RNG()
+    //debug code to check what the drawn card is.
     //console.log("Card_RNG:   ",Card_RNG())
     console.log("Constant_Card:   ", Current_Card)
+    Current_Card=Current_Card
     io.to("Team_A").to("Team_B").emit("card_drawn",Current_Card);
     
     currentTime = 60
@@ -307,12 +357,11 @@ io.on("connection", (socket) => {
     const msg = text
     socket.broadcast.emit("chat_transcribed_message", msg); //sends to all other clients to be displayed 2
     console.log(msg)
-    //replace these with a function later
+    // Debug code to see if the msg has been recieved and to check the card
     //socket.emit("chat_message", msg); // Send to all except sender
     //console.log("Current Card Keyword ==>", Current_Card.forbidden)
     //console.log("TEST CHAT MESSAGE")
     //console.log(Current_Card.forbidden)
-    //**remember to insert a try statement */
     const chat_msg = msg.toLowerCase().split(" ")
     console.log(chat_msg)
     chat_msg.forEach(function (element) {
@@ -327,7 +376,7 @@ io.on("connection", (socket) => {
           console.log("Capitalized Word for transcribe: ",capitalizedWord)
           Word_Checker(capitalizedWord)
         }catch{
-          console.log("No Card Drawn Yet")
+          console.log("No Card Drawn Yet(From transcribed msg)")
         }
         
       };
@@ -341,6 +390,7 @@ io.on("connection", (socket) => {
   socket.on("chat_message", (msg) => {
     console.log(`Message from ${socket.id}:`, msg);
     socket.broadcast.emit("chat_message", msg); // Send to all except sender
+    //code below is to test the output of the current card(forbidden word) and that the above event has been triggered
     //console.log("Current Card Keyword ==>", Current_Card.forbidden)
     //console.log("TEST CHAT MESSAGE")
     //console.log(Current_Card.forbidden)
@@ -355,42 +405,35 @@ io.on("connection", (socket) => {
         const capitalizedWord = firstLetterCap + remainingLetters
         console.log(capitalizedWord)
         i = chat_msg.shift();
-        try {
+        //try {
           Word_Checker(capitalizedWord)
-        } catch  {
-          console.log("No card drawn yet")
-        }
+        //} catch  {
+          //console.log("No card drawn yet(From chat msg)")
+        //}
       };
 
     });
+  });
+ 
 
     socket.on("Points_Updated",(scores) => {
       Team_Scores = Object.assign(scores)
       console.log("Curent Scores for this round",Team_Scores)
       console.log("Scores from the score variable", scores)
       io.emit("Update_Points",scores)
+      if (Team_Scores.Team_A_Points >5 || Team_Scores.Team_B_Points >5) {
+        app.get()
+      }
     })
 
     
 
     console.log("Curent Scores for this round",Team_Scores)
-  });
+  
 
   //Does not work for now dont use this (Was supposed to be for mic implementation but it did not work will replace soon)
   //depriciated code
-  //socket.on('transcriber', () =>{
-    //socket.join("transcriber")
-    //console.log("Transcriber id:", socket.id)
-  //});
-
-  //socket.on("Mic_Pressed", () =>{
-    //io.to("transcriber").emit('mic_pressed')
-  //});
-
-  //socket.on("Transcribed_Text", (text) => {
-    //io.emit("Transcribed_message" , text)
-  //});
-  //end of do not use stuff
+  //^^^code removed on 30/06/2025 if you still want it check the previous versions
   
   // Handle disconnections
   socket.on("disconnect", () => {
@@ -408,7 +451,7 @@ io.on("connection", (socket) => {
       Team_B_Count: Team_B.length,
       Card_Holder_Count: Card_Holder.length,
       guesser_count: guesser.length
-    };
+    }; //Might be useless
 
     io.emit("Capacity", Team_Player_Count);
     
@@ -417,6 +460,7 @@ io.on("connection", (socket) => {
   console.log("These socketids are in team B:",Team_B)
 });
 
+// Code below is supposed to trigger the local llm codes to generate new words might be used later on
 //const { spawn } = require("child_process");
 
 // Launch the Python transcriber
@@ -436,16 +480,13 @@ io.on("connection", (socket) => {
   //console.log(`Transcriber exited with code ${code}`);
 //});
 
+//code below is used to open up the server. just open http://localhost:3000 to access (Use on chrome for now. firefox, opera seems to break)
+//server.listen(port);
 
-server.listen(port);
-
-
-//server.listen(port, hostname, () => {
-  //console.log(`Server is running at http://${hostname}:${port}/`);
-//});
+//code below is used to open up the server. You have to uncomment the hostname part of the code first then comment the code above then it should work. Also uncomment the code below (Use on chrome for now. firefox, opera seems to break)
+server.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}/`);
+});
 
 /////////////PROBLEMS BOARD//////////////
-//**The current timer is wonky cause if you click draw card multiple times it will cause the timer to loop. Just make sure the button is unable to be presesd once a round has started**//
-//Fixed ^^^
-//**Seperate the Teams into 2 groups based on the stuff that was discussed**//
-//Done ^^^
+/////////////No current Problems//////////////
